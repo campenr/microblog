4c0bc7ce2bee95660c8b1670c74206f412d1b55a
campenr <richard@campen.co>
2016-12-08 16:46:35 -0500

    Fix license info in example_config.py

--- config/example_config.py
+++ config/example_config.py
@@ -1,7 +1,5 @@
 # Copyright 2016 Richard Campen
 # All rights reserved
-# This software is released under the Modified BSD license
-# See LICENSE.txt for the full license documentation
 
 """Flask based micro-blogging service.
 
